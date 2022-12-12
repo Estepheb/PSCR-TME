2d33b348e9953cfc33e1c77e7d79660f8097c7f6
Estepheb <114733551+Estepheb@users.noreply.github.com>
2022-12-12 18:18:44 +0100

    Add files via upload

--- TME7/src/pipe.cpp
+++ TME7/src/pipe.cpp
@@ -2,35 +2,50 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <vector>
+#include <string.h>
 
 int main (int argc, char ** argv) {
-    int pfd [2];
+    int pfd [2];//pfd[0] == lecture pfd[1] == ecriture
     pipe(pfd);
-
-    pid_t f1,f2,p;
-    p=getpid();
+    int i;
+    argv[0]=0;
     
-    
-    if((f1=fork())==0){
-        f1=getpid();
-        dup2(pfd[1],STDOUT_FILENO);
-        close (pfd[1]); close (pfd[0]);
-        execl("/bin/cat","cat","pipe.cpp",NULL);//execv pour utiliser un tableau
-        //read(pfd[0],&f2,sizeof(pid_t));//attends le pid de f2
-        //dup2(f1,f2);//remplace f2 par f1
+    char* argv1[20]={0};
+    char* argv2[20]={0};
 
-    return 0;
+    for(i=1;i<argc;i++){
+        argv1[i-1]=argv[i];
+         //std::cout<<argv[i]<<std::endl;    
+        if(strcmp(argv[i],"|")==0){
+            argv[i]=0;
+            argv1[i-1]=0;
+            break;}
     }
-    if((f2=fork())==0){
-        f2=getpid();
+    std::cout<<argc<<"\t"<<i<<std::endl;
 
-        dup2(pfd[0],STDIN_FILENO);
-        //write(pfd[1],&f2,sizeof(pid_t));
+
+    if((fork())==0){
+        dup2(pfd[1],STDOUT_FILENO);//l'ecriture correspond au Out de ce fork la sortie de ce exec sera donc lu au bout du pipe
+        
+        close (pfd[1]);
+        close (pfd[0]);
+
+        execv(argv1[0],argv1);//execv pour utiliser un tableau
+        return 0;
+    }
+
+    if((fork())==0){
+        dup2(pfd[0],STDIN_FILENO);//lit donc la sortie du premier fils 
+        //STDIN du fork2  = STDOUT du fork1 
         close (pfd[0]);
         close (pfd[1]);
         execl("/bin/wc","wc","-l",NULL);
         return 0;
     }
-    std::cout<<std::endl;
+
+    close (pfd[0]);//fermeture des pipes dans le main sinon les autres ne peuvent pas se fermer.
+    close (pfd[1]);
+    wait(0);//attend les deux processus
+    wait(0);
     return 0;
 }
