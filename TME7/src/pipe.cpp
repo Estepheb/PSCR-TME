02e689f25faf98386cbf519aaea5a3a9f35722ac
Estepheb <114733551+Estepheb@users.noreply.github.com>
2022-12-13 15:08:06 +0100

    Add files via upload

--- TME7/src/pipe.cpp
+++ TME7/src/pipe.cpp
@@ -5,26 +5,30 @@
 #include <string.h>
 
 int main (int argc, char ** argv) {
-    int pfd [2];//pfd[0] == lecture pfd[1] == ecriture
+    int pfd [2];
     pipe(pfd);
-    int i;
+
     argv[0]=0;
-    
-    char* argv1[20]={0};
-    char* argv2[20]={0};
+    int i,j;
+    char* argv1[10]={0};//premiere decomposition
+    char* argv2[10]={0};//deuxieme decomposition
 
     for(i=1;i<argc;i++){
-        argv1[i-1]=argv[i];
-         //std::cout<<argv[i]<<std::endl;    
+        argv1[i-1]=argv[i]; 
         if(strcmp(argv[i],"|")==0){
             argv[i]=0;
             argv1[i-1]=0;
-            break;}
+            break;
+        }
     }
-    std::cout<<argc<<"\t"<<i<<std::endl;
 
-
-    if((fork())==0){
+    i++;
+    for(j=0;i<argc;j++,i++){
+        argv2[j]=argv[i];
+    }
+    argv[j]=0;
+    
+ if((fork())==0){
         dup2(pfd[1],STDOUT_FILENO);//l'ecriture correspond au Out de ce fork la sortie de ce exec sera donc lu au bout du pipe
         
         close (pfd[1]);
@@ -39,7 +43,7 @@ int main (int argc, char ** argv) {
         //STDIN du fork2  = STDOUT du fork1 
         close (pfd[0]);
         close (pfd[1]);
-        execl("/bin/wc","wc","-l",NULL);
+        execv(argv2[0],argv2);
         return 0;
     }
 
@@ -48,4 +52,4 @@ int main (int argc, char ** argv) {
     wait(0);//attend les deux processus
     wait(0);
     return 0;
-}
+    }
